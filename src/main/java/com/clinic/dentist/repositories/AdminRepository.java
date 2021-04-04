f41ce1db29c26df85fb70054a157608b420b4485
User <tanya.shisha2001@gmail.com>
2021-04-04 21:53:00 +0300

    create AdminRepository

---
+++ src/main/java/com/clinic/dentist/repositories/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.clinic.dentist.repositories;
+
+import com.clinic.dentist.models.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    
+}
