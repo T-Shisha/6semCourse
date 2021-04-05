311201ec1835562037ee226e039da91b24cda065
User <tanya.shisha2001@gmail.com>
2021-04-05 12:30:01 +0300

    create  appointment repository

--- src/main/java/com/clinic/dentist/repositories/AppointmentRepository.java
+++ src/main/java/com/clinic/dentist/repositories/AppointmentRepository.java
@@ -1,9 +1,8 @@
 package com.clinic.dentist.repositories;
 
-import com.clinic.dentist.models.Dentist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DentistRepository extends JpaRepository<Dentist, Long> {
+public interface AppointmentRepository extends JpaRepository<AppointmentRepository, Long> {
 }
