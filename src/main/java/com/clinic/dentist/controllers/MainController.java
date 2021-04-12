b87f121b3287aece62edb74f7a3d19d323efc1d7
User <tanya.shisha2001@gmail.com>
2021-04-12 20:46:01 +0300

    create method to choose type of services

--- src/main/java/com/clinic/dentist/controllers/MainController.java
+++ src/main/java/com/clinic/dentist/controllers/MainController.java
@@ -1,16 +1,29 @@
 package com.clinic.dentist.controllers;
 
+import com.clinic.dentist.models.TypeServices;
+import com.clinic.dentist.services.TypeServicesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class MainController {
+    @Autowired
+    private TypeServicesService typeServicesService;
 
     @GetMapping("/")
     public String greeting(Model model) {
         return "main";
     }
 
+    @GetMapping("/types")
+    public String getTypes(Model model) {
+        List<TypeServices> types = typeServicesService.getAll();
+        model.addAttribute("types", types);
+        return "servicesType";
+    }
 }
