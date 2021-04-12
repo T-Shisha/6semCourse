5cbac2ecb091aedaf79ce34453468d2d9e59febc
User <tanya.shisha2001@gmail.com>
2021-04-12 21:08:17 +0300

    create method to show services for certain type

--- src/main/java/com/clinic/dentist/controllers/MainController.java
+++ src/main/java/com/clinic/dentist/controllers/MainController.java
@@ -1,11 +1,14 @@
 package com.clinic.dentist.controllers;
 
+import com.clinic.dentist.models.Maintenance;
 import com.clinic.dentist.models.TypeServices;
+import com.clinic.dentist.services.MaintenanceService;
 import com.clinic.dentist.services.TypeServicesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -14,6 +17,8 @@ import java.util.List;
 public class MainController {
     @Autowired
     private TypeServicesService typeServicesService;
+    @Autowired
+    private MaintenanceService maintenanceService;
 
     @GetMapping("/")
     public String greeting(Model model) {
@@ -25,5 +30,13 @@ public class MainController {
         List<TypeServices> types = typeServicesService.getAll();
         model.addAttribute("types", types);
         return "servicesType";
+    }
+
+    @GetMapping("/types/{id}")
+    public String getServices(@PathVariable(value = "id") long id, Model model) {
+        TypeServices typeServices= typeServicesService.findById(id);
+        List<Maintenance> services =typeServicesService.getSortedAscendingMaintenancesByPrice(typeServices) ;
+        model.addAttribute("services", services);
+        return "services";
     }
 }
